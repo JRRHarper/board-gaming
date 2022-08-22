047a65a977bf9a36df6f3e3c8599efc857953564
earthchild316 <earthchild316@gmail.com>
2022-08-22 14:27:57 +0100

    added first routes to spring boot project

---
+++ src/main/java/com/example/BoardGameApi/BoardGameApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.BoardGameApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardGameApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardGameApiApplication.class, args);
+	}
+
+}
